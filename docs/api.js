6f310948bce1d248570b61229242e08e3a9d7b93
Stefan Hedman <schteppe@gmail.com>
2013-10-12 13:35:22 +0200

    added docs

---
+++ docs/api.js
@@ -0,0 +1,12 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Line",
+        "Point",
+        "Polygon",
+        "Scalar"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
