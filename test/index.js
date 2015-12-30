02defe7c1294dea43810f869a6c42d4695fdbfe7
Stefan Hedman <schteppe@gmail.com>
2015-12-30 18:52:11 +0100

    Initial work. Saved a couple of kb :)

---
+++ test/index.js
@@ -0,0 +1,49 @@
+var polyDecomp = require('../src');
+
+var concave = [
+    [-1, 1],
+    [-1, 0],
+    [1, 0],
+    [1, 1],
+    [0.5, 0.5]
+];
+
+var circle = [],
+    N = 10;
+for(var i=0; i<N; i++){
+    var angle = Math.PI/N*i;
+    circle.push([Math.cos(angle),Math.sin(angle)]);
+}
+
+exports.decomp = function(test){
+    var circleConvexes = polyDecomp.decomp(circle);
+    test.equal(circleConvexes.length, 1);
+    var concaveConvexes = polyDecomp.decomp(concave);
+    test.equal(concaveConvexes.length, 2);
+    test.done();
+};
+
+exports.isSimple = function(test){
+    var notSimple = [
+        [-1,-1],
+        [ 0, 0],
+        [ 1, 1],
+        [ 0, 2],
+        [-1, 1],
+        [ 0, 0],
+        [ 1,-1]
+    ];
+
+    test.ok(polyDecomp.isSimple(concave));
+    test.ok(polyDecomp.isSimple(circle));
+    test.ok(!polyDecomp.isSimple(notSimple));
+    test.done();
+};
+
+exports.quickDecomp = function(test){
+    var result = polyDecomp.quickDecomp(circle);
+    test.equal(result.length, 1);
+    var convexResult = polyDecomp.quickDecomp(concave);
+    test.equal(convexResult.length, 2);
+    test.done();
+};
